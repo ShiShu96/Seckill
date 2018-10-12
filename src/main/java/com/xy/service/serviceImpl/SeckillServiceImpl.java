46390d1c923a5adc441767b206be954c2f560974
Will <shushi@nyu.edu>
2018-10-11 21:12:13 -0400

    Update SeckillServiceImpl.java

--- src/main/java/com/xy/service/serviceImpl/SeckillServiceImpl.java
+++ src/main/java/com/xy/service/serviceImpl/SeckillServiceImpl.java
@@ -119,4 +119,32 @@ public class SeckillServiceImpl implements SeckillService {
 
 
     }
+    
+    @Override
+	public SeckillExecution executeSeckillProcedure(long seckillId, long userPhone, String md5) {
+		if (md5 == null || !md5.equals(getMD5(seckillId))) {
+			return new SeckillExecution(seckillId, SeckillStateEnum.DATA_REWRITE);
+		}
+		Date killTime = new Date();
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("seckillId", seckillId);
+		map.put("phone", userPhone);
+		map.put("killTime", killTime);
+		map.put("result", null);
+		// 执行存储过程，result被赋值
+		try {
+			seckillDao.killByProcedure(map);
+			// 获取result
+			int result = MapUtils.getInteger(map, "result", -2);
+			if (result == 1) {
+				SuccessKilled sk = successKilledDao.queryByIdWithSeckill(seckillId, userPhone);
+				return new SeckillExecution(seckillId, SeckillStateEnum.SUCCESS, sk);
+			} else {
+				return new SeckillExecution(seckillId, SeckillStateEnum.stateOf(result));
+			}
+		} catch (Exception e) {
+			logger.error(e.getMessage(), e);
+			return new SeckillExecution(seckillId, SeckillStateEnum.INNER_ERROR);
+		}
+	}
 }
